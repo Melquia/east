065fd4a026e5afffb492d2b1bad6dfe21117d0dd
Melquia <dermelquiades@hotmail.de>
2015-10-05 02:19:04 -0500

    This is a commit test of the project.

--- amasaV1/amasaV1.c
+++ amasaV1/amasaV1.c
@@ -2,7 +2,7 @@
  * amasaV1.c
  *
  * Created: 10/4/2015 10:17:17 PM
- *  Author: Owner
+ *  Author: Melquia
  */ 
 
 
@@ -12,13 +12,13 @@ int main(void)
 {
 	unsigned char a,g;
 	g=0;
-	const unsigned char three[10]= {"orasle"};
+	const unsigned char three[10]= {"The test of GIT"};
 	for (a=0; a<10;a++)
 	{
 		g++;
 	}
     while(1)
     {
-        //TODO:: Please write your application code 
+        
     }
 }
