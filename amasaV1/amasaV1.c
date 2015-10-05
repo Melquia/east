18d9486f474aab7ecdc539b177223d71cb9b9e51
Melquia <dermelquiades@hotmail.de>
2015-10-05 01:59:54 -0500

    Initial commits

---
+++ amasaV1/amasaV1.c
@@ -0,0 +1,24 @@
+/*
+ * amasaV1.c
+ *
+ * Created: 10/4/2015 10:17:17 PM
+ *  Author: Owner
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+	unsigned char a,g;
+	g=0;
+	const unsigned char three[10]= {"orasle"};
+	for (a=0; a<10;a++)
+	{
+		g++;
+	}
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
